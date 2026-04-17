697996bb006872d12ea2e33e1bca3e900a66c8d4
Nani7172kumar <kumaraswamymarisarla@gmail.com>
2026-04-17 14:55:12 +0530

    day3 add Products and cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,12 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
 import Login from './components/Login'
-import Product from '../../backend/models/Product'
+import Addproduct from './components/Addproduct'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
+
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,10 +17,17 @@ export default function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/add-product' element={
-          <ProductRoute>
+        <Route path="/add-product" element={
+          <ProtectedRoute>
             <Addproduct/>
-            </ProductRoute>}/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
+          </ProtectedRoute>
+        }/>
+
       </Routes>
     </BrowserRouter>
   )
